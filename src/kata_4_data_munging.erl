8c225db9658f0f43bb313b91e3a649cd8497f4f8
mhurd <mjwhurd@gmail.com>
2015-04-15 13:49:18 +0100

    Added min/max and range helper functions

--- src/kata_4_data_munging.erl
+++ src/kata_4_data_munging.erl
@@ -22,10 +22,38 @@ process({ok, IoDevice}) ->
 process({error, Reason}) ->
   ?debugFmt("Read file failed: ~p~n", [Reason]).
 
+min_max([H|T]) ->
+  min_max(T, H , H).
+min_max([H|T], Min, Max) when H =< Min -> min_max(T, H, Max);
+min_max([H|T], Min, Max) when H >= Max -> min_max(T, Min, H);
+min_max([H|T], Min, Max) when (H > Min) and (H < Max) -> min_max(T, Min, Max);
+min_max([], Min, Max) -> {Min, Max}.
+
+-ifdef(EUNIT).
+min_max_test() ->
+  [?assertEqual({1,7}, min_max([1,3,6,7])),
+  ?assertEqual({1,1}, min_max([1])),
+  ?assertEqual({-9,100}, min_max([-4,2,-9,100])),
+  ?assertEqual({3,7}, min_max([7,3,6,6]))].
+-endif.
+
+range(Numbers) ->
+  case min_max(Numbers) of
+    {Min, Max} -> Max - Min
+  end.
+
+-ifdef(EUNIT).
+range_test() ->
+  [?assertEqual(6, range([1,3,6,7])),
+  ?assertEqual(0, range([1])),
+  ?assertEqual(109,  range([-4,2,-9,100])),
+  ?assertEqual(4, range([7,3,6,6]))].
+-endif.
+
 process_line(IoDevice) ->
   case file:read_line(IoDevice) of
     {ok, Data} ->
-      ?debugFmt("~p~n", [Data]),
+      ?debugFmt("~p", [string:tokens(Data, " ")]),
       process_line(IoDevice);
     {error, Reason} -> ?debugFmt("Read line failed: ~p~n", [Reason]);
     eof -> ?debugMsg("End of file reached.")
